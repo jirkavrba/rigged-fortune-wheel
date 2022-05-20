544da5fae76c87069f453f4eff3c06622ecbf31a
Jiří Vrba <jiri@vrba.dev>
2022-05-20 19:20:06 +0200

    Extract prices to a separate component

--- src/webapp/src/components/PriceList.tsx
+++ src/webapp/src/components/PriceList.tsx
@@ -1,5 +1,6 @@
 import {FC, useContext} from "react";
 import {AppContext} from "../App";
+import Price from "./Price";
 
 interface PriceListProps {
     onSelect?: (id: string) => void,
@@ -16,15 +17,9 @@ const PriceList: FC<PriceListProps> = ({onSelect, selected}) => {
 
     return (
         <div className="p-5">
-            <ul>
-                { entries.map(
-                    (entry, i) =>
-                        <li className={`flex flex-row items-center p-5 bg-white rounded-xl my-5 border cursor-pointer transition transform ${selected === entry.id ? 'text-black ring-4 ring-black text-black scale-105 shadow-xl' : 'text-gray-600'}`} key={i} onClick={() => select(entry.id)}>
-                            <div className="w-8 h-8 rounded-full mr-5" style={{ backgroundColor: entry.color }}></div>
-                            <div className="font-black">{entry.title}</div>
-                        </li>
-                ) }
-            </ul>
+            {entries.map(entry =>
+                <Price key={entry.id} selected={selected === entry.id} title={entry.title} color={entry.color} onClick={() => select(entry.id)} />
+            )}
         </div>
     );
 }
